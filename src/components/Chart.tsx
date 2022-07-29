fd13c2bbf57ddab8f9f1df23582d55e3793c2f0e
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-07-29 16:40:12 +0200

    css for the chart adjusted

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./chart.css";
 import { ChartInfo } from "../types/chartTypes";
 
 function Chart({ amount, day }: ChartInfo) {
+  const [onHover, setOnHover] = useState(false)
+
+  const handleMouse = () => {
+    setOnHover((prev) => !prev)
+  }
+  // React.MouseEvent<HTMLDivElement, MouseEvent>
   return (
-    <div className="bar__chart">
-      <span className="bar__chart-amount">${amount}</span>
+    <div className="bar__chart" onMouseEnter={handleMouse} onMouseLeave={handleMouse}>
+      <span className="bar__chart-amount" style={{
+        display: !onHover ? "none" : "block"
+      }}>${amount}</span>
       <div className="bar__chart-height" style={{ height: `${amount}%` }}></div>
   <p className="bar__chart-day">{day}</p>
     </div>
