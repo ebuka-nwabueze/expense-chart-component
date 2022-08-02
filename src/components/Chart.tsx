d8ba19d9b6a80ec0f228188068eb4065f47503a8
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-08-02 11:25:02 +0200

    added the variable length of the amount

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "./chart.css";
 import { ChartInfo } from "../types/chartTypes";
+import { calculateNewValue } from "@testing-library/user-event/dist/utils";
 
 function Chart({ amount, day }: ChartInfo) {
   const [onHover, setOnHover] = useState(false)
@@ -15,14 +16,20 @@ function Chart({ amount, day }: ChartInfo) {
     // setOnHover(false)
   }
 
+  const styles = onHover ? {
+    display: "block",
+    bottom: `calc(${amount + 20}%)`
+  } as React.CSSProperties : {}
+
   // React.MouseEvent<HTMLDivElement, MouseEvent>
   return (
     <div className="bar__chart" onMouseEnter={handleMouse} onMouseLeave={handleMouseOut}>
-      <span className="bar__chart-amount" style={{
-        display: onHover ? "block" : "none"
-      }}>${amount}</span>
+      {/* <span className="bar__chart-amount" style={{
+        display: onHover ? "hidden" : "hidden"
+      }}>${amount}</span> */}
+      <span className="bar__chart-amount" style={styles}>${amount}</span>
       <div className="bar__chart-height" style={{ height: `${amount}%` }}></div>
-  <p className="bar__chart-day">{day}</p>
+      <p className="bar__chart-day">{day}</p>
     </div>
   );
 }
